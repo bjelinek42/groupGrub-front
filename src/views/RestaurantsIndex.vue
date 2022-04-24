e13deb590ea53a435ef5a8b0a8fc2e9b2684ce46
ben <bjelinek42@gmail.com>
2022-04-24 13:48:35 -0600

    add my restaurants to restraunt index

--- src/views/RestaurantsIndex.vue
+++ src/views/RestaurantsIndex.vue
@@ -4,7 +4,8 @@ export default {
   data: function () {
     return {
       message: "Restaurant List",
-      restaurants: []
+      restaurants: [],
+      userRestaurants: []
     };
   },
   created: function () {
@@ -14,9 +15,11 @@ export default {
     indexRestaurants: function () {
       axios.get(`/restaurants`).then(response => {
         console.log('index restaurants', response.data)
-        this.restaurants = response.data
+        this.restaurants = response.data.restaurants
+        this.userRestaurants = response.data.user_restaurants
+        console.log(this.userRestaurants)
       })
-    }
+    },
   },
 };
 </script>
@@ -28,6 +31,12 @@ export default {
       <p>{{ restaurant.name }}</p>
       <p>{{ restaurant.address }}</p>
       <p><img v-bind:src="restaurant.image"></p>
+    </div>
+    <h2>My Restaurants</h2>
+    <div v-for="userRestaurant in userRestaurants" v-bind:key="userRestaurant">
+      <p>{{ userRestaurant.name }}</p>
+      <p>{{ userRestaurant.address }}</p>
+      <p><img v-bind:src="userRestaurant.image"></p>
     </div>
   </div>
 </template>
