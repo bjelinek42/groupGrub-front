439d522865e9ce210aba73a55fe6c9688b0bf479
ben <bjelinek42@gmail.com>
2022-05-02 10:00:11 -0600

    more to search page, img link, reviews link

--- src/views/RestaurantsSearch.vue
+++ src/views/RestaurantsSearch.vue
@@ -31,14 +31,24 @@ export default {
       <h1>Name: {{ restaurant.name }}</h1>
       <h1>Type: {{ restaurant.category.name }}, {{ restaurant.subcategory[0].name }}</h1>
       <h1>Address: {{ restaurant.address }}</h1>
-      <h1>Rating: {{ restaurant.rating }}/5</h1>
+      <h1>Rating: {{ restaurant.rating }}/5 <a v-bind:href="restaurant.web_url" target="_blank">Reviews</a></h1>
       <h1>Price: {{ restaurant.price_level }}</h1>
-      <!-- <h1>Hours: {{ restaurant.hours.week_ranges }}</h1> -->
-      <div v-for="cuisine in restaurant.cuisine" v-bind:key="cuisine.key">
-        <h1>Cuisines: {{ cuisine.name }}</h1>
+      <h1>Hours: Sunday: {{ restaurant.hours.week_ranges[0] }}
+        Monday: {{ restaurant.hours.week_ranges[1] }}
+        Tuesday: {{ restaurant.hours.week_ranges[2] }}
+        Wednesday: {{ restaurant.hours.week_ranges[3] }}
+        Thursday: {{ restaurant.hours.week_ranges[4] }}
+        Friday: {{ restaurant.hours.week_ranges[5] }}
+        Saturnday: {{ restaurant.hours.week_ranges[6] }}
+      </h1>
+      <h1>Cuisines:</h1>
+      <div v-for=" cuisine in restaurant.cuisine" v-bind:key="cuisine.key">
+        <h2>{{ cuisine.name }}</h2>
       </div>
       <!-- <h1>Dietary Restrictions: {{ restaurant.dietary_restrictions }}</h1> -->
-      <h2>Reserve a Table: <img v-bind:src="restaurant.reserve_info.provider_img" v-bind:alt="None"></h2>
+      <h2>Reserve a Table:<a v-bind:href="restaurant.reserve_info.url" target="_blank"><img
+            v-bind:src="restaurant.reserve_info.provider_img" v-bind:alt="None" style="width:300px"></a>
+      </h2>
     </div>
   </div>
 </template>
