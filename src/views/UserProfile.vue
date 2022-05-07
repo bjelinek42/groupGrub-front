61c184920bffd7cab28674b50e00df17e616007c
ben <bjelinek42@gmail.com>
2022-05-07 17:40:42 -0600

    boostrap cards for profile

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -39,12 +39,25 @@ export default {
     <h2>Username: {{ this.user.name }}</h2>
     <h2>Group: {{ this.group.name }} <button @click="groupPage()">View Group Page</button></h2>
     <button @click="addRestaurant()">Add Restaurant to Favorites</button>
-    <div v-for="restaurant in restaurants" v-bind:key="restaurant">
+    <div class="card-group">
+      <div class="card" v-for="restaurant in restaurants" v-bind:key="restaurant">
+        <div v-if="restaurant.image"><img v-bind:src="restaurant.image"></div>
+        <div class="card-body">
+          <h5 class="card-title">{{ restaurant.name }}</h5>
+          <p class="card-text">{{ restaurant.address }}</p>
+          <p class="card-text">{{ restaurant.cuisines }}</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+    <!-- <div v-for="restaurant in restaurants" v-bind:key="restaurant">
       <p>{{ restaurant.name }}</p>
       <p>{{ restaurant.cuisines }}</p>
       <p>{{ restaurant.address }}</p>
       <p><img v-bind:src="restaurant.image"></p>
-    </div>
+    </div> -->
   </div>
 </template>
 
