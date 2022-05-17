4b123abe28165e7f0d21dc819f0760f805975eba
ben <bjelinek42@gmail.com>
2022-05-17 15:26:56 -0600

    configure for heroku

--- src/main.js
+++ src/main.js
@@ -1,25 +1,15 @@
 import { createApp } from 'vue'
-// import Vue from 'vue'
-// import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"
-// import "bootstrap-vue/dist/bootstrap-vue.css"
 
-// Vue.use(BootstrapVue)
-
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://desolate-shore-45884.herokuapp.com/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
 }
-
-// new Vue({
-//   el: '#app',
-//   render: h => h(App)
-// })
 
 createApp(App).use(router).mount('#app')
