06ddd0aba63ae61afff725f6c4ad2dc6967fb453
ben <bjelinek42@gmail.com>
2022-08-19 10:10:12 -0600

    correct spelling on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,7 @@ export default {
   <div class="home">
     <h1>Welcome to groupGrub!</h1>
     <h2>We help you find a place to enjoy with a group,</h2>
-    <h2>without all the back-and-forth descision making!</h2>
+    <h2>without all the back-and-forth decision making!</h2>
     <h3>To get started, navigate to the signup page <button @click="toSignUp()">here</button></h3>
     <h3>Or if you already have an account, you and log in <button @click="toLogIn()">here</button></h3>
   </div>
